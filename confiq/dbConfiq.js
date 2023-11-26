2ba074cb46ede9a161b575c3b5ef38825dd41b18
Sohel Rana <expartdesigns5@gmail.com>
2023-11-26 23:59:02 +0600

    deploy

---
+++ confiq/dbConfiq.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+let {USER_NAME,PASSWORD,DATABASE_NAME}=process.env
+
+function dbConnection() {
+    mongoose
+        .connect(`mongodb+srv://sohel:sohel@mern-state.ier4ijb.mongodb.net/mern-state?retryWrites=true&w=majority`)
+        .then(() => console.log("Database Connected!"));
+}
+
+module.exports = dbConnection;
